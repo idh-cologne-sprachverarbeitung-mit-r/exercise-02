09fd9c6409d37488ce80ade5ace8ddc34efc6be0
ariannabienati <abienati@smail.uni-koeln.de>
2020-05-04 20:13:20 +0200

    first attempt

--- R/exercise.R
+++ R/exercise.R
@@ -4,27 +4,27 @@ a <- 5
 
 
 # b) Please create a variable called `b` that contains the character sequence `Hello`.
-
+b <- "Hello!"
 
 # c) Please create a variable called `c` that contains a vector with 5 elements (of your choice).
-
+c <- c(12,23,45,85,8)
 
 # d) Please create a variable called `d` that contains the numbers from 1 to 1000.
-
+d <- c(1:1000)
 
 # e) Create a variable `e` that contains logical values (i.e., true or false). The values should reflect whether the same position in vector `d` is even. I.e., every second position should contain `TRUE`.
-
+e <- d%%2==0
 
 # f) Create a variable `f` that adds contains *only* the even numbers between 1 and 1000.
-
+f <- d[d%%2==0] 
 
 # g) Assign the length of vector `f` to a variable `g`.
-
+g <- c(f)
 
 # h) Assign the mean of vector `f` to a variable `h`.
-
+h <- mean(f)
 
 # i) The file `R/exercise.R` contains a variable assignment `i0`. Find a way to define a variable `i`, such that it contains the elements of `i0` *in reverse order*.
-
+i <- rev(i0)
 i0 <- c("self", "future", "your", "to", "write", "you", "that", "letter", 
 "love", "a", "is", "Documentation")
